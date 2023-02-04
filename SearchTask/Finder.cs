744d2e7b1701af4ecf2d12014569c246c2f7319f
Kirill Davydov <d4vydovk@gmail.com>
2023-02-04 16:09:15 +0300

    add test class

--- SearchTask/Finder.cs
+++ SearchTask/Finder.cs
@@ -8,5 +8,17 @@ namespace SearchTask
 {
     public class Finder
     {
+        public void FindElementsForSum(List<uint> list, ulong sum, out int start, out int end)
+        {
+            if (list == null)
+            {
+                throw new NullReferenceException("List reference not set to an instance of an object.");
+            }
+            if (list.Count == 0)
+            {
+                throw new InvalidOperationException("List contains no elements");
+            }
+            start = end = 0;
+        }
     }
 }
