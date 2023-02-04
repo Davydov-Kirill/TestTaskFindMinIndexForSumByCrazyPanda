2e1ad8e3406e97925e08f158253702c8bf16dec1
Kirill Davydov <d4vydovk@gmail.com>
2023-02-04 20:22:57 +0300

    add method implementation

--- SearchTask/Finder.cs
+++ SearchTask/Finder.cs
@@ -1,14 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SearchTask
+﻿namespace SearchTask
 {
     public class Finder
     {
         public void FindElementsForSum(List<uint> list, ulong sum, out int start, out int end)
+        {
+            start = end = 0;
+            CheckSetValues(list, sum, ref start, ref end);
+            ulong result;
+            for (int i = 0; i < list.Count; i++)
+            {
+                result = 0;
+                for (int j = i; j < list.Count; j++)
+                {
+                    if (result < sum) result += list[j];
+                    if (result == sum && sum != 0)
+                    {
+                        start = i;
+                        end = j + 1;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private static void CheckSetValues(List<uint> list, ulong sum, ref int start, ref int end)
         {
             if (list == null)
             {
@@ -18,7 +33,11 @@ namespace SearchTask
             {
                 throw new InvalidOperationException("List contains no elements");
             }
-            start = end = 0;
+            if (sum is 0 && list.Contains(0))
+            {
+                start = list.IndexOf(0);
+                end = start++;
+            }
         }
     }
 }
